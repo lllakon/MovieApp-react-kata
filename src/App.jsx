5acb8c31c1d46bb04c6e38ba6f60e98dc9bf9a44
lllakon <CoffeWhite58@gmail.com>
2025-01-14 22:26:22 +0200

    css styles
    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import { useState } from 'react';
 import HeadTabs from './components/HeadTabs/HeadTabs';
 import SearchInput from './components/SearchInput/SearchInput';
 import MoviesList from './components/MoviesList/MoviesList';
-import { useState } from 'react';
 import './index.css';
 
-// Tabs должны передавать свой статус в фетч
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [currentTab, setCurrentTab] = useState('search');
