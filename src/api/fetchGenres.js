3ccafb8825bfd71556f66f623ed1cac0ac24d4a7
lllakon <CoffeWhite58@gmail.com>
2025-01-19 21:17:50 +0200

    -
    -

--- src/api/fetchGenres.js
+++ src/api/fetchGenres.js
@@ -1,4 +1,4 @@
-const apiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOGZlMGQ2ZGU3N2Y5ZDhlNjA3ZGYxODhiMzM5NWJkZCIsIm5iZiI6MTczNjcwMTU4Ny40MDYwMDAxLCJzdWIiOiI2NzgzZjY5MzIxOGZkNTdhY2Y0ZjE3ODMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.vz6fDS7E2x3d3mgmvrs6kmyHreSiL4YjqEZLbusn6BY';
+const apiToken = import.meta.env.VITE_API_TOKEN;
 const API_URL = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
 
 export const fetchGenres = async () => {
